bfac2a3ee0fb16d02f22035be36c69e201fb40aa
dell_laptop_JSE <seeuncheong@gmail.com>
2022-03-29 10:51:23 +0900

    0329_mybatis Dao, vo 설정

---
+++ src/hrd/vo/Member.java
@@ -0,0 +1,26 @@
+package hrd.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor		// 기본생성자
+@AllArgsConstructor		// 모든 프로퍼티가 인자인 생성자
+@ToString
+
+public class Member {
+
+	private int custNo;
+	private String custName;
+	private String phone;
+	private String addr;
+	private Date joinDate;
+	private String grade;
+	private String city; 
+}
